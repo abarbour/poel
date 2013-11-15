f5bbe9b6047736021b5ad7b8a320ed6e46162b26
Andy Barbour <andy.barbour@gmail.com>
2013-11-15 10:27:24 -0800

    Initial commit. This is really just a pipe dream at the moment.

---
+++ R/inp.R
@@ -0,0 +1,132 @@
+inp <- function(){
+  #   # This is the input file of FORTRAN77 program "poel06" for modeling
+  #   # coupled deformation-diffusion processes based on a multi-layered (half-
+  #   # or full-space) poroelastic media induced by an injection (pump) of
+  #   # from a borehole or by a (point) reservoir loading.
+  #   #
+  #   # by R. Wang,
+  #   # GeoForschungsZentrum Potsdam
+  #   # e-mail: wang@gfz-potsdam.de
+  #   # phone 0049 331 2881209
+  #   # fax 0049 331 2881204
+  #   #
+  #   # Last modified: Potsdam, Nov, 2006
+  #   #
+  #   ##############################################################
+  #   ##                                                          ##
+  #   ## Cylindrical coordinates (Z positive downwards!) are used ##
+  #   ## If not others specified, SI Unit System is used overall! ##
+  #   ##                                                          ##
+  #   ## Tilt is positive when the upper end of a borehole tilt-  ##
+  #   ## meter body moves away from the pumping well.             ##
+  #   ##                                                          ##
+  #   ##############################################################
+  #   #
+  #   #        SOURCE PARAMETERS
+  #   #        =================
+  #   # 1. start and end depth [m] of the injection (pump) screen, and well radius [m];
+  #   # 2. number of input data lines for the source time function;
+  #   # 3. listing of the source time series.
+  #   #-------------------------------------------------------------------------------
+  #   %(s_start_depth)g %(s_end_depth)g  %(s_radius)g                 |dble: s_start_depth, s_end_depth, s_radius;
+  #   #-------------------------------------------------------------------------------
+  #   2
+  #   #-------------------------------------------------------------------------------
+  #   # no    time    source_function
+  #   # [-]   [hour]  [m^3/h for injection(+)/pump(-) rate]
+  #   #-------------------------------------------------------------------------------
+  #   %(source_function)s
+  #   #-------------------------------------------------------------------------------
+  #   #
+  #   #        RECEIVER PARAMETERS
+  #   #        ===================
+  #   # 1. observation depth [m]
+  #   # 2. switch for equidistant trace steping (1/0 = yes/no)
+  #   # 3. number of distance samples [m] (<= nrmax defined in "peglobal.h")
+  #   # 4. if equidistant, start trace distance, end trace distance; else list of
+  #   #    trace distances (all >= 0 and ordered from small to large!)
+  #   # 5. length of time window [h], number of time samples
+  #   #-------------------------------------------------------------------------------
+  #   %(receiver_depth)g              |dble: r_depth;
+  #   %(sw_equidistant)i              |int: sw_equidistant;
+  #   %(no_distances)i                |int: no_distances;
+  #   %(str_distances)s               |dble: d_1,d_n; or d_1,d_2, ...;
+  #   %(t_window)s %(no_t_samples)i   |dble: t_window; int: no_t_samples;
+  #   #-------------------------------------------------------------------------------
+  #   #
+  #   #        WAVENUMBER INTEGRATION PARAMETERS
+  #   #        =================================
+  #   # 1. relative accuracy (0.01 for 1%% error) for numerical wavenumber integration;
+  #   #-------------------------------------------------------------------------------
+  #   %(accuracy)s                           |dble: accuracy;
+  #   #-------------------------------------------------------------------------------
+  #   #
+  #   #        OUTPUTS A: DISPLACEMENT
+  #   #        =======================
+  #   # 1. select the 3 displacement time series (1/0 = yes/no)
+  #   # 2. file names of these 3 time series
+  #   #-------------------------------------------------------------------------------
+  #   %(sw_t_files_1_3)s                                        |int: sw_t_files(1-3);
+  #   %(t_files_1_3)s                                   |char: t_files(1-3);
+  #   #-------------------------------------------------------------------------------
+  #   #
+  #   #        OUTPUTS B: STRAIN TENSOR & TILT
+  #   #        ===============================
+  #   # 1. select strain time series (1/0 = yes/no): Ezz, Err, Ett, Ezr, Ert, Etz
+  #   #    (6 tensor components) and Tr (= -dur/dz, the radial component of the
+  #   #    vertical tilt). Note Tt can be derived from Etz and Ut
+  #   # 2. file names of these 7 time series
+  #   #-------------------------------------------------------------------------------
+  #   %(sw_t_files_4_10)s      |int: sw_t_files(4-10);
+  #   %(t_files_4_10)s |char: t_files(4-10);
+  #   #-------------------------------------------------------------------------------
+  #   #
+  #   #        OUTPUTS C: PORE PRESSURE & DARCY VELOCITY
+  #   #        =========================================
+  #   # 1. select pore pressure and Darcy velocity time series (1/0 = yes/no):
+  #   #    P (excess pore pressure), Vz, Vr, Vt (3 Darcy velocity components)
+  #   # 2. file names of these 4 time series
+  #   #-------------------------------------------------------------------------------
+  #   %(sw_t_files_11_14)s                              |int: sw_t_files(11-14);
+  #   %(t_files_11_14)s                         |char: t_files(11-14);
+  #   #-------------------------------------------------------------------------------
+  #   #
+  #   #        GLOBAL MODEL PARAMETERS
+  #   #        =======================
+  #   # 1. switch for surface conditions:
+  #   #    0 = without free surface (whole space),
+  #   #    1 = unconfined free surface (p = 0),
+  #   #    2 = confined free surface (dp/dz = 0).
+  #   # 2. number of data lines of the layered model (<= lmax as defined in
+  #   #    "peglobal.h") (see Note below)
+  #   #-------------------------------------------------------------------------------
+  #   %(isurfcon)i                   |int: isurfcon
+  #   %(no_model_lines)i             |int: no_model_lines;
+  #   #-------------------------------------------------------------------------------
+  #   #
+  #   #        MULTILAYERED MODEL PARAMETERS
+  #   #        =============================
+  #   #
+  #   # no depth[m] mu[Pa]    nu    nu_u   B     D[m^2/s]   Explanations
+  #   #-------------------------------------------------------------------------------
+  #   %(model)s
+  #   #--------------------------end of all inputs------------------------------------
+  #   
+  #   Note for the model input format and the step-function approximation for models
+  #   with depth gradient:
+  #     
+  #     The surface and the upper boundary of the half-space as well as the interfaces
+  #   at which the poroelastic parameters are continuous, are all defined by a single
+  #   data line; All other interfaces, at which the poroelastic parameters are
+  #   discontinuous, are all defined by two data lines (upper-side and lower-side values). 
+  #   This input format would also be needed for a graphic plot of the
+  #   layered model. Layers which have different parameter values at top and bottom,
+  #   will be treated as layers with a constant gradient, and will be discretised to a
+  #   number of homogeneous sublayers. Errors due to the discretisation are limited
+  #   within about 5%% (changeable, see peglobal.h).
+}
+
+generator <- function(x, ...){ UseMethod("generator") }
+generator.inp <- function(x, ...){
+  #
+}
